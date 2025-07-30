9875fed7d80a6a17c6653f565e70bf7b016be63b
Stephen Akuffo <sa.akuffo@gmail.com>
2025-07-30 13:56:34 -0400

    code update

--- terraform-deploy/main.tf
+++ terraform-deploy/main.tf
@@ -13,7 +13,7 @@ provider "vsphere" {
   user           = var.vsphere_user
   password       = var.vsphere_password
   vsphere_server = var.vsphere_server
-  
+
   # If you have a self-signed cert
   allow_unverified_ssl = true
 }
@@ -156,7 +156,7 @@ resource "vsphere_virtual_machine" "ubuntu_nginx" {
 
   num_cpus = var.vm_cpu
   memory   = var.vm_memory
-  
+
   guest_id = data.vsphere_virtual_machine.template.guest_id
 
   scsi_type = data.vsphere_virtual_machine.template.scsi_type
@@ -175,43 +175,34 @@ resource "vsphere_virtual_machine" "ubuntu_nginx" {
 
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
-
-    customize {
-      linux_options {
-        host_name = "${var.vm_name}-${count.index + 1}"
-        domain    = "local"
-      }
-
-      network_interface {
-        ipv4_address = length(var.static_ips) > 0 ? var.static_ips[count.index] : ""
-        ipv4_netmask = length(var.static_ips) > 0 ? var.netmask : 0
-      }
-      
-      ipv4_gateway = length(var.static_ips) > 0 ? var.gateway : ""
-    }
   }
 
   # Wait for the VM to be ready
-  wait_for_guest_net_timeout = 5  # Wait up to 5 minutes for network
-  wait_for_guest_ip_timeout  = 5  # Wait up to 5 minutes for IP
-  wait_for_guest_net_routable = false  # Don't wait for routable
+  wait_for_guest_net_timeout  = 10    # Wait up to 10 minutes for network
+  wait_for_guest_ip_timeout   = 10    # Wait up to 10 minutes for IP
+  wait_for_guest_net_routable = false # Don't wait for routable
 
   cdrom {
     client_device = true
   }
 
-  connection {
-    type     = "ssh"
-    user     = var.ssh_username
-    password = var.ssh_password
-    host     = self.default_ip_address
-    timeout  = "10m"
-  }
-
-  # Wait for SSH to be ready
+  # Install nginx after VM is ready
   provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = var.ssh_username
+      password = var.ssh_password
+      host     = self.default_ip_address
+      timeout  = "10m"
+    }
+
     inline = [
-      "echo 'VM is ready for provisioning'"
+      "sudo apt-get update",
+      "sudo apt-get install -y nginx",
+      "sudo systemctl start nginx",
+      "sudo systemctl enable nginx",
+      "sudo ufw allow 'Nginx Full'",
+      "echo 'Nginx installed successfully'"
     ]
   }
 
